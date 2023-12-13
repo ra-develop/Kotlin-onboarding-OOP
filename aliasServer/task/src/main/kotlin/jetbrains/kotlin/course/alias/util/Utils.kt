afc4bf059d135c5e657ba37302ed201343618187
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-14 00:43:42 +0100

    Stage: aliasServer: Task 5/16 finished

--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/util/Utils.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/util/Utils.kt
@@ -1,1 +1,8 @@
 package jetbrains.kotlin.course.alias.util
+
+typealias Identifier = Int
+
+class IdentifierFactory(private var counter: Identifier = 0) {
+    fun uniqueIdentifier(): Identifier = counter++
+}
+
