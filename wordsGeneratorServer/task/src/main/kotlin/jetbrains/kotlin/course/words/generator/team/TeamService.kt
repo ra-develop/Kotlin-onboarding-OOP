d6aa0481d29b4e7e5dd061b46dc3cdfa8a1d7a49
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-15 00:44:39 +0100

    Stage: Word Generator: Task 4/7 finished

--- wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/team/TeamService.kt
+++ wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/team/TeamService.kt
@@ -4,5 +4,15 @@ import org.springframework.stereotype.Service
 
 @Service
 class TeamService {
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        val generatedTeams = List(teamsNumber) {
+            Team()
+        }
+        generatedTeams.forEach { team -> teamsStorage.put(team.id, team) }
+        return generatedTeams
+    }
+
+    companion object {
+        val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
+    }
 }
