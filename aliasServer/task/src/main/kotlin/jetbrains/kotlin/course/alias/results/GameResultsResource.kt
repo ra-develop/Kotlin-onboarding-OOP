aa4d3d14cb468082d70fffe8168594ac1dadd514
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-14 21:54:18 +0100

    Stage: aliasServer: Task 16/16 finished

--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsResource.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsResource.kt
@@ -1,9 +1,8 @@
 package jetbrains.kotlin.course.alias.results
 
 import alias.JsTeam
+import jetbrains.kotlin.course.alias.util.toGameResult
 import org.springframework.web.bind.annotation.*
-
-typealias GameResult = String
 
 // We can not use a typealias here because the Spring framework can not parse it
 class GameJsResult : ArrayList<JsTeam>()
@@ -13,7 +12,7 @@ class GameJsResult : ArrayList<JsTeam>()
 class GameResultsResource(val service: GameResultsService) {
     @CrossOrigin
     @PostMapping("/save")
-    fun saveGameResults(@RequestBody result: GameJsResult): Unit = TODO("Not implemented yet")
+    fun saveGameResults(@RequestBody result: GameJsResult) = service.saveGameResults(result.toGameResult())
 
     @CrossOrigin
     @GetMapping("/all")
--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/results/GameResultsService.kt
@@ -1,10 +1,26 @@
 package jetbrains.kotlin.course.alias.results
 
+import jetbrains.kotlin.course.alias.team.Team
+import jetbrains.kotlin.course.alias.team.TeamService
 import org.springframework.stereotype.Service
+
+typealias GameResult = List<Team>
 
 @Service
 class GameResultsService {
-    fun saveGameResults(result: GameResult): Unit = TODO("Not implemented yet")
+    fun saveGameResults(result: GameResult) {
+        if (result.isEmpty()) {
+            error("Result must be not empty")
+        }
+        if (!result.any {TeamService.teamsStorage.containsKey(it.id) }) {
+            error("All team ids from the result must be present in the TeamService.teamsStorage")
+        }
+        gameHistory.add(result)
+    }
 
-    fun getAllGameResults(): List<GameResult> = TODO("Not implemented yet")
+    fun getAllGameResults(): List<GameResult> = gameHistory
+
+    companion object {
+        val gameHistory: MutableList<GameResult> = mutableListOf()
+    }
 }
