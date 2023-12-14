ed365453bb323e616df3b105b50d36f813deb5bd
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-14 16:26:04 +0100

    Stage: aliasServer: Task 7/16 finished

--- aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ aliasServer/task/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,25 @@
 package jetbrains.kotlin.course.alias.team
 
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
 
 @Service
 class TeamService {
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    private val identifierFactory: IdentifierFactory = IdentifierFactory()
+
+    companion object {
+        val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
+    }
+
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        val teamsForOneRound = List(teamsNumber) {
+            Team(identifierFactory.uniqueIdentifier())
+        }
+        teamsForOneRound.forEach {
+            teamsStorage[it.id] = it
+        }
+        return teamsForOneRound
+    }
 }
