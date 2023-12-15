45150515edcdc9f60f59fe24510df2e30c59d31b
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-15 18:37:55 +0100

    Stage: Word Generator: Task 7/7 finished

--- wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/results/GameResultsService.kt
+++ wordsGeneratorServer/task/src/main/kotlin/jetbrains/kotlin/course/words/generator/results/GameResultsService.kt
@@ -1,10 +1,30 @@
 package jetbrains.kotlin.course.words.generator.results
 
+import jetbrains.kotlin.course.words.generator.team.Team
+import jetbrains.kotlin.course.words.generator.team.TeamService
 import org.springframework.stereotype.Service
+
+typealias GameResult = List<Team>
 
 @Service
 class GameResultsService {
-    fun saveGameResults(result: GameResult): Unit = TODO("Not implemented yet")
 
-    fun getAllGameResults(): List<GameResult> = TODO("Not implemented yet")
+    companion object{
+        val gameHistory : MutableList<GameResult> = mutableListOf()
+    }
+    fun saveGameResults(result: GameResult): Unit {
+        if (result.isEmpty()) {
+            error("Result must not be empty")
+        }
+        result.forEach {
+            if (!TeamService.teamsStorage.containsKey(it.id)) {
+                error("All team ids in result must be present in TeamService.teamsStorage")
+            }
+        }
+        gameHistory.add(result)
+    }
+
+    fun getAllGameResults(): List<GameResult> {
+        return gameHistory.reversed()
+    }
 }
