e1ae53121117118bbc9592b534c47626059dda67
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-23 23:19:12 +0100

    Stage: Codenames: task 14/15 finished add

---
+++ codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/card/Card.kt
@@ -0,0 +1,5 @@
+package jetbrains.kotlin.course.codenames.card
+
+data class Card(val data: CardData, val state: CardState) {
+
+}
--- codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/card/CardModel.kt
+++ codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/card/CardModel.kt
@@ -1,4 +1,11 @@
 package jetbrains.kotlin.course.codenames.card
 
 interface  CardData
+
 data class WordCardData(val word: String): CardData
+
+enum class CardState {
+    Front,
+    Back
+}
+
