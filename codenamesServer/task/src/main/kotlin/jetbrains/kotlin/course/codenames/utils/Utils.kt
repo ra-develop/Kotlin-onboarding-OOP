3f056b9629ba42f8a7b8aa14966076ecc2e710fd
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-23 22:54:47 +0100

    Stage: Codenames: task 10/15 finished

--- codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/utils/Utils.kt
+++ codenamesServer/task/src/main/kotlin/jetbrains/kotlin/course/codenames/utils/Utils.kt
@@ -23,18 +23,21 @@ object Utils {
         { "The total number of cards in the game must be: $TOTAL_NUMBER" }
     }
 
-    private var previousAttempts: MutableList<List<KeyCardCell>> = mutableListOf()
+    private val previousAttempts: MutableList<List<KeyCardCell>> = mutableListOf()
+
     val uniqueKeyCardGenerator: KeyCardGenerator = KeyCardGenerator {
-        var keyCards: List<KeyCardCell> = listOf()
-        while (keyCards in previousAttempts) {
-            keyCards = KeyCardType.values().map { key ->
+        var keyCardCells: List<KeyCardCell> = listOf()
+
+        do {
+            keyCardCells = KeyCardType.values().map { key ->
                 List(key.number) {
                     KeyCardCell(key)
                 }
             }.flatten().shuffled()
-        }
-        previousAttempts.add(keyCards)
-        keyCards
+        } while (keyCardCells in previousAttempts)
+
+        previousAttempts.add(keyCardCells)
+        keyCardCells
     }
 
 }
