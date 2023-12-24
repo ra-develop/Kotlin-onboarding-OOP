1aaa3c7e761308014cb14da64d359e808876733f
Rashid Amanzholov <r.amanzholov@nexid.it>
2023-12-25 00:38:31 +0100

    Stage: Memory Trainer: Task 5/6: finished

--- memoryTrainerServer/task/src/main/kotlin/jetbrains/kotlin/course/card/trainer/card/CardService.kt
+++ memoryTrainerServer/task/src/main/kotlin/jetbrains/kotlin/course/card/trainer/card/CardService.kt
@@ -1,10 +1,28 @@
 package jetbrains.kotlin.course.card.trainer.card
 
+import jetbrains.kotlin.course.card.trainer.util.countries
 import org.springframework.stereotype.Service
 
 @Service
 class CardService {
-    fun getNextCard(): Card = TODO("Not implemented yet")
+    fun getNextCard(): Card {
+        require(cards.isNotEmpty()) { "Some error"}
+        return cards.removeFirst()
+    }
 
-    fun startNewGame(): Card = TODO("Not implemented yet")
+    fun startNewGame(): Card {
+        cards = generateNewCardsSequence()
+        return getNextCard()
+    }
+
+    companion object {
+        val randomCardGenerator = CardSequenceGenerator {
+            countries.map { (capital, country) ->
+                Card(Front(capital), Back(country))
+            }.shuffled()
+        }
+
+        var cards = generateNewCardsSequence()
+        private fun generateNewCardsSequence(): MutableList<Card> = randomCardGenerator.generateCards().toMutableList()
+    }
 }
